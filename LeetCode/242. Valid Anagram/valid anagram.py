b17c7ccdd8a2062be569a985fb2f14ae2cc1a678
Tausif Ahmed Turzo <turzo09@yahoo.com>
2024-10-21 16:44:45 +0600

    Create valid anagram.py

---
+++ LeetCode/242. Valid Anagram/valid anagram.py
@@ -0,0 +1,20 @@
+s="anagram"
+t="nagaram"
+
+def valid_anagram(s,t):
+    if(len(s)!=len(t)):
+        return False
+    
+    countS,countT = {},{}
+
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i],0)
+        countT[t[i]] = 1 + countT.get(t[i],0)
+
+    for c in countS:
+        if countS[c]!= countT.get(c,0):
+            return False
+        
+    return True
+
+print(valid_anagram(s,t))
