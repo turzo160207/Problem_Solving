bdcd7a8408e9d5bd50a45631f8ac88d12605147a
Tausif Ahmed Turzo <turzo09@yahoo.com>
2024-11-18 13:37:36 +0600

    solved leetcode problem 424

---
+++ LeetCode/424. Longest Repeating Character Replacement/longest repeating character replacement.py
@@ -0,0 +1,23 @@
+s = "ababc"
+k = 2
+
+def character_replacement(s,k):
+    count = {}
+    res = 0
+    maxf = 0
+    l = 0
+
+    for r in range(len(s)):
+        count[s[r]] = 1 + count.get(s[r],0)
+
+        maxf = max(maxf, count[s[r]])
+
+        if (r-l+1) - maxf > k:
+            count[s[l]] -=1
+            l+=1
+        
+        res = max(res, r-l+1)
+
+    return res
+
+print(character_replacement(s,k))
